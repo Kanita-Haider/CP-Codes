3f65e45d45c3218b9ed88f06e0a9885a85913359
Kanita-Haider <kanitahaider44@gmail.com>
2022-11-21 22:13:05 +0600

    prac

---
+++ temp.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi acos(-1)
+#define ms(a,b) memset(a, b, sizeof a)
+#define sp setprecision
+#define fasterInOut ios::sync_with_stdio(false); cin.tie(0);
+#define debug(x) cout<<@<<" = "<<x<<endl;
+#define MAX 100005
+
+
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        ll x,ans;
+        cin>>x;
+
+        ans=x*(x-1);
+        cout<<ans<<endl;
+
+    }
+
+    return 0;
+}
