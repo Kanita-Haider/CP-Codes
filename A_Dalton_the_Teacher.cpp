6044cf05f8992914343a285713152c023fb30c7f
Kanita-Haider <kanitahaider44@gmail.com>
2023-07-30 17:47:17 +0600

    coding interview folder added

---
+++ A_Dalton_the_Teacher.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#define   ll        long long
+#define   ull       unsigned long long 
+#define   mp        make_pair
+#define   pb        push_back
+#define   ff        first
+#define   ss        second
+#define   nl         "\n"
+#define   pii       pair <int, int>
+#define   pll       pair <ll , ll > 
+#define   pi        acos(-1.0)
+#define mset(a, b) memset(a, b, sizeof(a))
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+using namespace std;
+
+//freopen("E:/vs code/input.txt", "r", stdin);
+//freopen("E:/vs code/output.txt", "w", stdout);
+//cout << fixed << setprecision(3) <<nl;
+
+/*ll md=LLONG_MAX;
+ll power(ll val,ll pw)
+{
+    if(pw==0)return 1;
+    if(pw%2==0)
+    {
+        ll tmp=power(val,pw/2)%md;
+        return (tmp*tmp)%md;
+    }
+    else return (val*power(val,pw-1))%md;
+}
+*/
+
+/* STL SYNTAX
+vector<int>v ;
+cin>>x;
+v.push_back(x);
+
+pair<int, int>p ;
+vector<pair<int,int>>v;
+v.pb(mp(a,b));
+
+vector< pair<int,int>>v(n);
+ for(int i=0; i<n;i++){
+     cin>>v[i].first>>v[i].second; }
+
+set<int, greater<int> > s1;  set<int>st ;
+st.insert( x ) ;
+set<int> :: iterator itr;
+for (itr = st.begin(); itr != st.end(); itr++) 
+        cout << *itr << " ";
+    
+map<int, int>mp ;
+map<char, int> :: iterator it ;
+for(it = mp.begin() ; it!=mp.end(); it++)
+        cout<<(*it).second<<nl ;
+*/
+
+
+int main()
+{
+ FAST;
+int t=0;
+cin>>t;
+while(t--){
+int n,ans=0,cnt=0;
+cin>>n;
+vector<int>v(n+5);
+int arr[n+5];
+
+for(int i=1;i<=n;i++){
+    cin>>arr[i];
+    if(arr[i]==i) cnt++;
+}
+if(cnt%2==0)
+cout<<cnt/2<<nl;
+else cout<<(cnt+1)/2<<nl;
+}
+return 0;
+} 
+
---
+++ cracking the coding interview/A1.sum_of_n_num.cpp
@@ -0,0 +1,10 @@
+
+
+int sum(int n){
+    int res;
+    res=(n*(n+1))/2;
+    return res;
+}
+
+
+
